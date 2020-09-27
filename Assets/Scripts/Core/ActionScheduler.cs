b39fea2156d2c92d076bf81c6b96f6dcb8410bd7
thxForu <xavavka12345@gmail.com>
2020-09-27 17:00:02 +0300

    add IAction

--- Assets/Scripts/Core/ActionScheduler.cs
+++ Assets/Scripts/Core/ActionScheduler.cs
@@ -12,7 +12,7 @@ namespace RPG.Core
             if (currentAction == action) return;
             if (currentAction != null)
             {
-                action.Cancel();
+                currentAction.Cancel();
             }
             currentAction = action;
         }
