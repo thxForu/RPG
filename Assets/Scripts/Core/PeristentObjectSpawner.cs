ab90f9948a3e1a520859a838d2ff39cb4344e5a7
thx4u <xavavka12345@gmail.com>
2020-11-16 22:40:02 +0200

    fade in and fade Out

---
+++ Assets/Scripts/Core/PeristentObjectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PeristentObjectSpawner : MonoBehaviour
+    {
+        [SerializeField] private GameObject persistentPrefab;
+        
+        
+        private static bool hasSpawned = false;
+
+        private void Awake()
+        {
+            if (hasSpawned) return;
+            SpawnPersistentObjects();
+            hasSpawned = true;
+        }
+
+        private void SpawnPersistentObjects()
+        {
+            GameObject persistentObject = Instantiate(persistentPrefab);
+            DontDestroyOnLoad(persistentObject);
+        }
+    }   
+}
