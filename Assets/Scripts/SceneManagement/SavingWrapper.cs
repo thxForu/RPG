379802261eb8330fcb40a0518547c6001f651852
thx4u <xavavka12345@gmail.com>
2020-11-26 23:02:54 +0200

    saving system

---
+++ Assets/Scripts/SceneManagement/SavingWrapper.cs
@@ -0,0 +1,31 @@
+﻿using RPG.Saving;
+using UnityEngine;
+
+namespace RPG.SceneManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        private const string defaultSaveFile = "save";
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+
+            if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Load()
+        {
+            GetComponent<SavingSystem>().Load(defaultSaveFile);
+        }
+        private void Save()
+        {
+            GetComponent<SavingSystem>().Save(defaultSaveFile);
+        }
+    }
+}
