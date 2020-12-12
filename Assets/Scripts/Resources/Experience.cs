a7b6bafac367c1c019a48126548679443d4f81b0
thx4u <xavavka12345@gmail.com>
2020-12-12 19:16:34 +0200

    experience

---
+++ Assets/Scripts/Resources/Experience.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Resources
+{
+    public class Experience : MonoBehaviour
+    {
+        [SerializeField] private float experiencePoints;
+
+        public void GainExperience(float experience)
+        {
+            experiencePoints += experience;
+        }
+    }
+}
